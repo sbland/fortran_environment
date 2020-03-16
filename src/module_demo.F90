abdd641cb1d2fed5e180c4e198ced7e8cf35a606
Sam Bland <sb2457@its.york.ac.uk>
2020-03-16 14:46:45 +0000

    Update example - add dependecies

--- src/module_demo.F90
+++ src/module_demo.F90
@@ -1,4 +1,6 @@
 module module_demo
+    use module_demo_external
+
     implicit none
 
 contains
@@ -6,6 +8,7 @@ contains
         implicit none
         integer,intent(in) :: arg1
         integer,intent(out) ::  arg2
-        arg2 = arg1 + 1
+        ! arg2 = arg1 + 1
+        call rab(arg1, arg2)
     end subroutine ra
 end module module_demo
---
+++ src/module_demo_external.F90
@@ -0,0 +1,11 @@
+module module_demo_external
+    implicit none
+
+contains
+    subroutine rab(arg1,  arg2)
+        implicit none
+        integer,intent(in) :: arg1
+        integer,intent(out) ::  arg2
+        arg2 = arg1 + 10
+    end subroutine rab
+end module module_demo_external
